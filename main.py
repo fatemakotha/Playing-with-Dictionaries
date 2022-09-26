32ef745c4fc8f303c45265150f248b67a73a4bdc
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-26 17:16:26 +0000

    Using one function and passing two dictionaries as inputs

--- main.py
+++ main.py
@@ -1,7 +1,53 @@
 dictionary = {
     "name": "Kotha",
     "work": "Student",
-    "age": 25,
+    "age": "medium",
     "height": "above average",
 }
+
+news = {
+    "1": ".......",
+    "2": "hihihi",
+    "3": "+++++++",
+    "4": "/ / / / ",
+}
+
 print(dictionary)
+print("\n")
+print(news)
+print("\n")
+print("\n")
+print("\n")
+
+
+
+#function that adds the string 123 to every item:
+
+def add(dic):
+    dic["counrty"] = "Bangladesh"
+    print(dic)
+
+    
+add(dictionary)
+print("\n")
+add(news)
+
+
+# def test(dictt):
+#     result = list(map(list, dictt.items()))
+#     return result    
+
+# color_dict = {1 : 'red', 2 : 'green', 3 : 'black', 4 : 'white', 5 : 'black'}
+# print("\nOriginal Dictionary:")
+# print(color_dict)
+# print("Convert the said dictionary into a list of lists:")
+# result1 = test(color_dict)
+# print(result1)
+
+# color_dict = {'1' : 'Austin Little', '2' : 'Natasha Howard', '3' : 'Alfred Mullins', '4' : 'Jamie Rowe'}
+# print("\nOriginal Dictionary:")
+# print(color_dict)
+# print("Convert the said dictionary into a list of lists:")
+# result2 = test(color_dict)
+# print(result2)
+
